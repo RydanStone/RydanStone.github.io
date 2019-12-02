16635d05a4e75808ea443d779e0e62bc436a4969
RydanStone <57165658+RydanStone@users.noreply.github.com>
2019-12-02 10:43:18 -0800

    start in nightmode

--- jscript/nightmode.js
+++ jscript/nightmode.js
@@ -1,11 +1,12 @@
 document.body.style.backgroundColor = localStorage.getItem('nm');
 
+if (localStorage.getItem('nm') == null || undefined) {
+        localStorage.setItem('nm', 'on');
+}
 if(localStorage.getItem('nm') == 'on'){
   document.body.classList.toggle('nightmode');
 }
-else if (localStorage.getItem('nm') == null || undefined) {
-        localStorage.setItem('nm', 'off');
-}
+
 
 document.querySelector('[data-switch-contrast]').addEventListener('click', function() {
   document.body.classList.toggle('nightmode');
