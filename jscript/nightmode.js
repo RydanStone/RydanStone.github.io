a4c740c452c3105e7e53467d858ec06a237208cc
RydanStone <57165658+RydanStone@users.noreply.github.com>
2019-12-04 11:03:43 -0800

    Proper Commenting

--- jscript/nightmode.js
+++ jscript/nightmode.js
@@ -1,12 +1,19 @@
+//load nightmode(nm) item
 document.body.style.backgroundColor = localStorage.getItem('nm');
 
+//checks if this is their first time on the site or cleared their cookies
+//sets nightmode on if true
 if (localStorage.getItem('nm') == null || undefined) {
         localStorage.setItem('nm', 'on');
 }
+
+//checks to see if they had nightmode on
+//if they did, it toggles nightmode class onto objects
 if(localStorage.getItem('nm') == 'on'){
   document.body.classList.toggle('nightmode');
 }
 
+//Handles any click on nightmode button and switches the value to the opposite of the current value
 document.querySelector('[data-switch-contrast]').addEventListener('click', function() {
   document.body.classList.toggle('nightmode');
   if(localStorage.getItem('nm') == 'on'){
